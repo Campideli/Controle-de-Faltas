312eaa97566f80baf302964d9828c3af9361f63d
Campideli <soufercamp@gmail.com>
2024-04-04 01:20:09 -0300

    Login e Cadastro funcionais

---
+++ firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBJiNBV0ITB-xrprSJsbZPJ3C0R65YZg4A",
+    authDomain: "faltei-f4c7d.firebaseapp.com",
+    projectId: "faltei-f4c7d",
+    storageBucket: "faltei-f4c7d.appspot.com",
+    messagingSenderId: "958235014116",
+    appId: "1:958235014116:web:9f7b1a1e20507354a1cdf1"
+};
+firebase.initializeApp(firebaseConfig);
--- mail.js
+++ mail.js
@@ -1,69 +1,102 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyC9b4UAZfRNza0L2GEWAMfi7lB3NySTuFA",
-  authDomain: "controle-de-faltas-ad6ee.firebaseapp.com",
-  databaseURL: "https://controle-de-faltas-ad6ee-default-rtdb.firebaseio.com",
-  projectId: "controle-de-faltas-ad6ee",
-  storageBucket: "controle-de-faltas-ad6ee.appspot.com",
-  messagingSenderId: "928864718863",
-  appId: "1:928864718863:web:e27bd81046e9f5dfd9137a",
-  measurementId: "G-E9TEYX40DP"
-  };
-  
-  firebase.initializeApp(firebaseConfig);
-  
-  const auth = firebase.auth()
-  const database = firebase.database()
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    alert("Usuário logado!");
+  }
+});
 
-  var ControleFaltaDB = firebase.database().ref("ControleFalta");
+function validateLogin(){
+  var email = form.email().value;
+  var password = form.password().value;
+  if (isNaN(password) == false || validateEmail(email) == false) {
+    form.botaologin().setAttribute("disabled", "disabled");
+  } else {
+    form.botaologin().removeAttribute("disabled");
+  }
+  if (validateEmail(email)) {
+    form.recupera().style.color = "#EDC18E";
+  } else {
+    form.recupera().style.color = "#edc18e8f";
+  }
+}
 
-  function validatePassword(){
-    var name = document.getElementById("name").value;
-    var password = document.getElementById("password").value;
-    var confirm_password = document.getElementById("confirm_password").value;
-
-    if (password == confirm_password && password.length >= 6 && confirm_password.length >= 6 && name != "" && password != "" && confirm_password != "") {
-      document.getElementById("botao").removeAttribute("disabled");
-    } else {
-      document.getElementById("botao").setAttribute("disabled", "disabled");
-    }
-    
-    if (password == confirm_password){
-      document.getElementById("conferem").style.color = "#EDC18E";
-    } else {
-      document.getElementById("conferem").style.color = "#edc18e8f";
-    }
-    
-    if (password.length >= 6 || confirm_password.length >= 6){ 
-      document.getElementById("minimo").style.color = "#EDC18E";
-    } else {
-      document.getElementById("minimo").style.color = "#edc18e8f";
-    }
-    
-    if (name != "" || password != "" || confirm_password != "") {
-      document.getElementById("vazio").style.color = "#EDC18E";
-    } else {
-      document.getElementById("vazio").style.color = "#edc18e8f";
-    }
-  }    
-  
-  function salvar(){
-    var name = document.getElementById("name").value;
-    var password = document.getElementById("password").value;
-    var confirm_password = document.getElementById("confirm_password").value;
-    saveMessages(name, password, confirm_password);
-    alert("Cadastro realizado com sucesso!");
+function validateRegister(){
+  var email = form.email().value;
+  var password = form.password().value;
+  var confirm_password = form.confirm_password().value;
+  if (password == confirm_password && password.length >= 6 && confirm_password.length >= 6 && isNaN(email) && isNaN(password) && isNaN(confirm_password) && validateEmail(email)) {
+    form.botao().removeAttribute("disabled");
+  } else {
+    form.botao().setAttribute("disabled", "disabled");
   }
   
-  const saveMessages = (name, password, confirm_password) => {
-    var newControleFalta = ControleFaltaDB.push();
+  if (validateEmail(email)) {
+    form.valido().style.color = "#EDC18E";
+  } else {
+    form.valido().style.color = "#edc18e8f";
+  }
+  if (password == confirm_password && isNaN(password) && isNaN(confirm_password)) {
+    form.conferem().style.color = "#EDC18E";
+  } else {
+    form.conferem().style.color = "#edc18e8f";
+  }
   
-    newControleFalta.set({
-      name: name,
-      password: password,
-      confirm_password: confirm_password,
+  if (password.length >= 6){ 
+    form.minimo().style.color = "#EDC18E";
+  } else {
+    form.minimo().style.color = "#edc18e8f";
+  }
+  
+  if (isNaN(email) && isNaN(password) && isNaN(confirm_password)) {
+    form.vazio().style.color = "#EDC18E";
+  } else {
+    form.vazio().style.color = "#edc18e8f";
+  }
+}    
+
+const form = {
+  email: () => document.getElementById("email"),
+  password: () => document.getElementById("password"),
+  confirm_password: () => document.getElementById("confirm_password"),
+  botao: () => document.getElementById("botao"),
+  botaologin: () => document.getElementById("botaologin"),
+  recupera: () => document.getElementById("recupera"),
+  valido: () => document.getElementById("valido"),
+  conferem: () => document.getElementById("conferem"),
+  minimo: () => document.getElementById("minimo"),
+  vazio: () => document.getElementById("vazio")
+}
+
+function validateEmail(email) { 
+  const regex = /^[^\s@]+@[^\s@]+.[^\s@]+$/; 
+  return regex.test(email); 
+}
+
+function registrar(){
+  firebase.auth().createUserWithEmailAndPassword(form.email().value, form.password().value)
+    .then(() => {
+      console.log("Usuário cadastrado com sucesso!");
+      window.location.href = 'login.html';
+    })
+    .catch((error) => {
+      alert("Usuário já cadastrado!");
     });
-  };
-  
-  const getElementVal = (id) => {
-    return document.getElementById(id).value;
-  };
+}
+
+function login(){
+  firebase.auth().signInWithEmailAndPassword(form.email().value, form.password().value)
+    .then(() => {
+      console.log("Login successful");
+      window.location.href = 'home.html';
+    })
+    .catch((error) => {
+      alert("Usuário ou senha inválidos!");
+    });
+}
+
+function recuperarSenha(){
+  firebase.auth().sendPasswordResetEmail(form.email().value).then(() => {
+    alert("Email enviado com sucesso!");
+  }).catch((error) => {
+    alert("Email inválido!");
+  });
+}

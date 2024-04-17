4371661bafe683325da8d790df4915399328212b
Campideli <soufercamp@gmail.com>
2024-04-16 22:36:36 -0300

    Revert "login agora é index"
    This reverts commit 7bab464e326df51f00950cc0f68c95d2b791593f.

--- Paginas/Home/home.js
+++ Paginas/Home/home.js
@@ -1,6 +1,6 @@
 function logout(){
     firebase.auth().signOut().then(function(){
-        window.location.href = "/index.html";
+        window.location.href = "/Paginas/Registro/login.html";
     }).catch(function(error){
         alert('Erro ao deslogar!');
     });
